b6d6a2d3f0495fea972cee8d23dd0b2886ce2120
Zoë Knox <zoe@pixin.net>
2020-03-12 16:59:23 -0400

    Wrapper scripts to set up pieces more easily

---
+++ ingress.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+up() {
+    YAML1=ingress-nginx.yaml
+    YAML2=cloud-generic.yaml
+    if [ ! -f "$YAML1" ] || [ ! -f "$YAML2" ]; then
+        echo "Downloading the manifest"
+        curl -sL -o "$YAML1" https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.26.1/deploy/static/mandatory.yaml
+        curl -sL -o "$YAML2" https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.26.1/deploy/static/provider/cloud-generic.yaml
+    fi
+    echo "Install Ingress"
+    kubectl apply -f "$YAML1"
+    kubectl apply -f "$YAML2"
+
+    echo "Waiting for pods"
+    READY=""
+    while [ "$READY" != "1/1" ]; do
+        READY=$(kubectl get pods --all-namespaces -l app.kubernetes.io/name=ingress-nginx|grep -o "1/1")
+    done
+
+    echo "Waiting for external address"
+    LBADDR=""
+    while [ "$LBADDR" == "" ]; do
+      LBADDR=$(kubectl get svc -A -l app.kubernetes.io/name=ingress-nginx | grep -Eo "LoadBalancer\s+\S+\s+[0-9.]+\s+80:" | awk '{print $3}')
+    done
+    echo LoadBalancer "$LBADDR"
+
+    echo "Publishing A records"
+    az network dns record-set a add-record -z cloud.opennms.com --ttl 600 -g opennms-global -n '*.flows' -a "$LBADDR"
+}
+
+down() {
+    kubectl delete ns ingress-nginx
+    echo "Deleting A records"
+    az network dns record-set a delete -z cloud.opennms.com -g opennms-global -n '*.flows' -y
+}
+
+status() {
+    echo Ingress NGINX
+    kubectl get svc,pods --all-namespaces -l app.kubernetes.io/name=ingress-nginx
+}
+
+case "$1" in
+    up) up ;;
+    down) down ;;
+    status) status ;;
+    *) echo $(basename $0) '(up|down|status)' ;;
+esac
---
+++ system.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+
+up() {
+    ./cert-mgr.sh up
+    ./ingress.sh up
+
+    echo "Installing Jaeger CRDs"
+    kubectl apply -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/crds/jaegertracing.io_jaegers_crd.yaml
+
+    echo "Installing Drift"
+    kubectl apply -k aks
+
+    echo "Waiting for external Kafka address"
+    LBADDR=""
+    while [ "$LBADDR" = "" ]; do
+      LBADDR=$(kubectl get svc ext-kafka -n opennms | grep -Eo "LoadBalancer\s+\S+\s+[0-9.]+\s+" | awk '{print $3}')
+    done
+    echo LoadBalancer "$LBADDR"
+
+    echo "Publishing A records"
+    az network dns record-set a add-record -z cloud.opennms.com --ttl 600 -g opennms-global -n 'kafka.flows' -a "$LBADDR"
+}
+
+down() {
+    kubectl delete -k aks
+    ./ingress.sh down
+    ./cert-mgr.sh down
+}
+
+status() {
+    ./cert-mgr.sh status
+    ./ingress.sh status
+    kubectl get pod -n opennms
+
+    ADMINUSER=$(kubectl -n opennms get secret kafka-jaas -o jsonpath='{.data.KAFKA_ADMIN_USER}' | base64 -d)
+    PASSWORD=$(kubectl -n opennms get secret kafka-jaas -o jsonpath='{.data.KAFKA_ADMIN_PASSWORD}' | base64 -d)
+    echo "Kafka admin user: $ADMINUSER Password: $PASSWORD"
+
+    CLIENTUSER=$(kubectl -n opennms get secret kafka-jaas -o jsonpath='{.data.KAFKA_CLIENT_USER}' | base64 -d)
+    PASSWORD=$(kubectl -n opennms get secret kafka-jaas -o jsonpath='{.data.KAFKA_CLIENT_PASSWORD}' | base64 -d)
+    echo "Kafka client user: $CLIENTUSER Password: $PASSWORD"
+
+    PASSWORD=$(kubectl -n opennms get secret onms-passwords -o jsonpath='{.data.ELASTICSEARCH_PASSWORD}' | base64 -d)
+    echo "Elasticsearch password: $PASSWORD"
+
+    PASSWORD=$(kubectl -n opennms get secret onms-passwords -o jsonpath='{.data.GRAFANA_UI_ADMIN_PASSWORD}' | base64 -d)
+    echo "Grafana admin password: $PASSWORD"
+
+    PASSWORD=$(kubectl -n opennms get secret onms-passwords -o jsonpath='{.data.OPENNMS_UI_ADMIN_PASSWORD}' | base64 -d)
+    echo "OpenNMS admin password: $PASSWORD"
+}
+
+create() {
+    az group create --name "$GROUP" --location "$LOCATION"
+    az aks create --name opennms \
+    --resource-group "$GROUP" \
+    --service-principal "$APP_ID" \
+    --client-secret "$PASSWORD" \
+    --dns-name-prefix opennms \
+    --kubernetes-version 1.15.7 \
+    --location "$LOCATION" \
+    --node-count 4 \
+    --node-vm-size Standard_DS3_v2 \
+    --nodepool-name onmspool \
+    --network-plugin azure \
+    --network-policy azure \
+    --generate-ssh-keys \
+    --tags Environment=Development
+    az aks get-credentials --name opennms -g "$GROUP" --context "$GROUP"
+
+    sed -i -e "s/__EMAIL__/$EMAIL/" -e "s/__DOMAIN__/$DOMAIN/g" manifests/external-access.yaml
+}
+
+destroy() {
+    az aks delete --yes --name opennms -g "$GROUP"
+    az group delete --yes --name "$GROUP"
+}
+
+export GROUP=flowslab
+export LOCATION=centralus
+export DOMAIN="flows.cloud.opennms.com"
+export EMAIL="saas@opennms.com"
+
+case "$1" in
+    create) create ;;
+    destroy) destroy ;;
+    up) up ;;
+    down) down ;;
+    status) status ;;
+    *) echo $(basename $0) '(up|down|status)' ;;
+esac
